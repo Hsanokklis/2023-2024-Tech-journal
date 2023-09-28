aa07eb970399adec90ce2592d01949331fa8479e
Hannelore Sanokklis <113212665+Hsanokklis@users.noreply.github.com>
2023-09-28 12:43:01 -0400

    Add files via upload

---
+++ ClassExercise1.py
@@ -0,0 +1,40 @@
+"""
+Team: 
+Team members: Hannelore, Savannah, Lily, Adra
+
+Goal: Write a Python program that will convert any number, regarless of its base
+to its base-10, binary or hexadecmimal equivalent
+
+Assumptions: When possible, Python user-defined functiuons control the flow and
+execution of the code
+
+The options include:
+A: Binary to Hexadecmial
+B: Binary to Base-10
+C: Hexadecimal to Base-10
+D: Hexadecimal to Binary
+E: Base-10 to Binary
+F: Base-10 to Hexadecimal
+
+Steps:
+1. Provide conversion options to the user
+2. User inputs choice of conversation option
+3. Provide input for the value
+4. User enters an appropriate value
+5. Print resul
+6. stop
+"""
+
+"""
+BiToHex = 
+BiToTen = 
+HexToTen = 
+HexToBi = 
+TenToBi = 
+TenToHex = 
+
+"""
+
+print('Welcome to the conversion center!')
+print('Here is a list of conversion options, which one would like to use (enter the letter)?')
+print()
