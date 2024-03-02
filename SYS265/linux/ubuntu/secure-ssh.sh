02bf42cb3979e417187ca311dd79e999507ada78
Hsanokklis <hsanokklis@gmail.com>
2024-03-02 17:19:33 -0500

    seucre-ssh.sh commit

---
+++ SYS265/linux/ubuntu/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh 
+#author hsanokklis 
+#creates a new ssh user using $1 
+#adds a public key from the local repo or curled from the remote repo 
+#removes roots ability to ssh in 
+echo "ALL YOUR CODE GOES HERE" 
