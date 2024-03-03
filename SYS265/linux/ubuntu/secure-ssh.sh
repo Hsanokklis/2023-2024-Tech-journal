876e4b24200fcba6ee32c76599ac7899b21fec54
hsanokklis <hsanokklis@gmail.com>
2024-03-03 02:01:12 +0000

    final script

--- SYS265/linux/ubuntu/secure-ssh.sh
+++ SYS265/linux/ubuntu/secure-ssh.sh
@@ -3,7 +3,7 @@
 #creates a new ssh user using $1 
 #adds a public key from the local repo or curled from the remote repo 
 #removes roots ability to ssh in 
-echo "ALL YOUR CODE GOES HERE" 
+echo "Script will run once you input the sudo password -->" 
 
 #!/bin/bash
 
